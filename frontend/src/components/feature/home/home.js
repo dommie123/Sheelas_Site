51ca67cf5060e53efd11dbe49f4082be6c78ff17
Dominick Wiley <dman9532@gmail.com>
2024-01-06 01:32:06 -0700

    Added temp fix for sell button for now

--- frontend/src/components/feature/home/home.js
+++ frontend/src/components/feature/home/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { IconButton } from '@mui/material';
+import { Fab } from '@mui/material';
 import { Add } from '@mui/icons-material';
 
 import ItemList from './item-list/item-list';
@@ -8,7 +8,7 @@ import ItemList from './item-list/item-list';
 import { styled } from '@mui/material/styles';
 import './home.css';
 
-const CustomIconButton = styled(IconButton)({
+const CustomIconButton = styled(Fab)({
     backgroundColor: 'greenyellow',
     padding: '15px'
 })
@@ -22,7 +22,9 @@ export default function HomePage() {
 
     return (
         <div className='home-container'>
-            <ItemList />
+            <div className='home-content'>
+                <ItemList />
+            </div>
             <CustomIconButton className='add-item-button' onClick={handleAddItem}>
                 <Add />
             </CustomIconButton>
--- frontend/src/slices/item-slice.js
+++ frontend/src/slices/item-slice.js
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-import { showError } from "../utils/error";
 import { determineBackendURL } from "../AppConfig";
 
 export const getItems = createAsyncThunk(
